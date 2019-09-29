078c561f31973fd76022d4def79950a0790faabc
JustinVegaMadrigal-UNA <Justin.Vega.Madrigal@est.una.ac.cr>
2019-09-29 09:55:58 -0600

    Se hizo algo

---
+++ FaryvetLogisticSupport/FaryvetLogisticSupport/Pages/Conductores/IndexConductores.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FaryvetLogisticSupport.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace FaryvetLogisticSupport.Pages.Conductores
+{
+    public class IndexConductoresModel : PageModel
+    {
+        private readonly ApplicationDbContext _dbContext ;
+
+        public IndexConductoresModel (ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext ;
+        }
+
+        public IEnumerable <FARYVET_FLS_Conductor> FARYVET_FLS_Conductores { get; set; }
+        
+        public async Task OnGet ( )
+        {
+            FARYVET_FLS_Conductores = await _dbContext.FARYVET_FLS_Conductor.ToListAsync ( ) ;
+        }
+    }
+}
