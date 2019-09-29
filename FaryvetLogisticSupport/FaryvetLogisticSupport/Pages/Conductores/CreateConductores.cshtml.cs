84633e098a39781a7a1dee479bcf0301831dfe58
JustinVegaMadrigal-UNA <Justin.Vega.Madrigal@est.una.ac.cr>
2019-09-28 19:43:59 -0600

    Ahora se pueden crear "Conductores"

---
+++ FaryvetLogisticSupport/FaryvetLogisticSupport/Pages/Conductores/CreateConductores.cshtml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FaryvetLogisticSupport.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FaryvetLogisticSupport.Pages.Conductores
+{
+    public class CreateConductoresModel : PageModel
+    {
+        private readonly ApplicationDbContext _dbContext ;
+        public CreateConductoresModel (ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext ;
+        }
+        [BindProperty]
+        public FARYVET_FLS_Conductor FARYVET_FLS_Conductor { get; set; }
+        public void OnGet()
+        {
+
+        }
+
+        public async Task <IActionResult> OnPost ( )
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page ( ) ;
+            }
+            _dbContext.Add ( FARYVET_FLS_Conductor) ;
+            await _dbContext.SaveChangesAsync ( ) ;
+            return RedirectToPage ("../Index") ;
+        }
+    }
+}
