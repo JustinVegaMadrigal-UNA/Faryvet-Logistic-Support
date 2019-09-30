afde821612ed1923f28827ed355bb9cd8d45c413
JustinVegaMadrigal-UNA <Justin.Vega.Madrigal@est.una.ac.cr>
2019-09-29 21:17:40 -0600

    No se hizo nada

---
+++ FaryvetLogisticSupport/FaryvetLogisticSupport/Pages/Conductores/EditConductores.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FaryvetLogisticSupport.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FaryvetLogisticSupport.Pages.Conductores
+{
+    public class EditConductoresModel : PageModel
+    {
+        private readonly ApplicationDbContext _dbContext ;
+
+        public EditConductoresModel (ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext ;
+        }
+
+        [BindProperty]
+        public FARYVET_FLS_Conductor FARYVET_FLS_Conductor { get; set; }
+
+        public async Task OnGet(String numeroCedula)
+        {
+            Console.WriteLine(numeroCedula);
+            FARYVET_FLS_Conductor = await _dbContext.FARYVET_FLS_Conductor.FindAsync(numeroCedula) ;
+        }
+    }
+}
